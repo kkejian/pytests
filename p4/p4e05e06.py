d2cc2e74e7e34fd77e69024a192a1fba4c07cbfc
kkejian <kyle.kejian@googlemail.com>
2016-01-27 13:01:10 +0100

    Added bonus for Ex6

--- p4/p4e05e06.py
+++ p4/p4e05e06.py
@@ -4,12 +4,19 @@ D2 = copyDict(D)
 D['a'] = 99
 
 def addDict(d1,d2):
-    D=copyDict(d1)
-    for i in d2.keys(): D[i] = d2[i]
-    return D
+    if type(d1) == type(dict()):
+        D=copyDict(d1)
+        for i in d2.keys(): D[i] = d2[i]
+        return D
+    else:
+        return d1[:] + d2[:]
 
 print(list(D.items()))
 print(list(D2.items()))
 
 D3 = dict(zip('cde',range(1,4)))
 print(list(addDict(D,D3).items()))
+l1, l2 = list(range(3)), list(range(1,4)) 
+print(list(addDict(l1,l2)))
+print(l1)
+print(l2)
