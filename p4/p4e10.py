cd50009b02d49b70b2cdc5d1a3f041fe09bc1288
kkejian <kyle.kejian@googlemail.com>
2016-01-29 14:24:18 +0100

    new excercise added

---
+++ p4/p4e10.py
@@ -0,0 +1,15 @@
+import time, math
+start = time.time()
+def timetotal(func, *args, _rep=1000, **kargs):
+    start = time.time()
+    for i in range(_rep):
+        ret = func(*args, **kargs)
+    return (time.time() - start, ret)
+
+res = timetotal(math.sqrt, 9345)
+print(list(res))
+
+#def timebest(func, *args, _rep=5, **kargs):
+    #best = 
+
+
