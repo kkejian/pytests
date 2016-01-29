d5fecbeaa987c7bf37c58456833675e107a4a2b2
kkejian <kyle.kejian@googlemail.com>
2016-01-29 16:36:45 +0100

    Ex10 still WIP

--- p4/p4e10.py
+++ p4/p4e10.py
@@ -6,10 +6,19 @@ def timetotal(func, *args, _rep=1000, **kargs):
         ret = func(*args, **kargs)
     return (time.time() - start, ret)
 
-res = timetotal(math.sqrt, 9345)
+
+#def timebest(func, *args, _rep=5, **kargs):
+#    best = 2 ** 32
+#    for i in range(_rep):
+#        start = time.time()
+#        ret = func(*args, **kargs)
+#        elapsed = start - time.time()
+#        if elapsed < best: best = elapsed
+#    return (best, ret)
+    
+def timebesttotal(func, *args, _reppr=10000, _rounds=10, **kargs):
+    return min([timetotal(func, *args, _rep=_reppr  **kargs) for i in range(_rounds)])
+
+res = timebesttotal(math.sqrt, 9345)
 print(list(res))
-
-def timebest(func, *args, _rep=5, **kargs):
-    best = 
-
 
