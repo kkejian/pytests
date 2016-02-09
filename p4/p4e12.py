7a1460f11e8b134461452bc7686b2b04e82d7af2
kkejian <kyle.kejian@googlemail.com>
2016-02-09 10:21:59 +0100

    All functions for ex12

---
+++ p4/p4e12.py
@@ -0,0 +1,32 @@
+def facRec(n):
+    if n==0: return 1
+    else: return n*facRec(n-1)
+
+def facRed(n):
+    from functools import reduce
+    return (reduce(lambda x,y: x*y, range(1,n+1)) if n else 1)
+
+def facLoop(n):
+    start = 1
+    for i in range(1,n+1): start *= i
+    return start
+
+def facLib(n):
+    from math import factorial
+    return factorial(n)
+
+#print(facRec(3))
+#print(facRed(3))
+#print(facLoop(3))
+#print(facLib(3))
+#print(facRec(0))
+#print(facRed(0))
+#print(facLoop(0))
+#print(facLib(0))
+#print(facRec(1))
+#print(facRed(1))
+#print(facLoop(1))
+#print(facLib(1))
+
+import timeit
+min(timeit.repeat(stmt="[facRec(n) for n in range(100)]", number = 1000, repeat = 5))
